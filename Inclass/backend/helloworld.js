d1ecc9cda6ded7b3d4027edce7469c676ff0deeb
adnon444 <adnon444@yahoo.com>
2024-09-28 12:32:47 -0400

    labexercise2

---
+++ Inclass/backend/helloworld.js
@@ -0,0 +1,35 @@
+// this is the structure of a function
+// function ThisIsTheName(signature) {
+// Body
+// }
+function sayHello() {
+    console.log('Hello Adnon');
+  }
+  sayHello();
+  
+function sum (a, b) {
+  console.log(a + b);
+}
+
+function subtract (a, b) {
+  console.log(a - b);
+}
+
+function product (a, b) {
+  console.log(a * b);
+}
+
+function divide (a, b) {
+  console.log(a / b);
+}
+
+sum(1, 5)
+subtract(1, 5)
+product(1, 5)
+divide(1, 5)
+
+const str1 = 'Hello';
+const str2 = 'World';
+
+console.log(str1.concat(' ',str2));
+
